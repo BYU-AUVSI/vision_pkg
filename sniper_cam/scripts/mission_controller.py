39059de1479b2bb145bda8b960d4d415f54e92d2
Ephraim Kunz <ephraimkunz@me.com>
2018-04-02 09:11:53 -0600

    Bug fixes

--- sniper_cam/scripts/mission_controller.py
+++ sniper_cam/scripts/mission_controller.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 from Tkinter import *
 from ScrolledText import *
+from tkFileDialog import askopenfilename
+
 import rospy
 import roslib
 import signal
@@ -15,6 +17,10 @@ BOTTLE_DROP_MISSION_NAME = "bottle drop"
 TARGET_SEARCH_MISSION_NAME = "target search"
 OTHER_MISSION_NAME = "other"
 LAND_MISSION_NAME = "land"
+
+INIT_STATE = "Nothing planned or sent yet"
+PATH_GENERATED_STATE = "Planned path"
+PATH_SEND_STATE = "Path sent to plane"
 
 MISSION_TYPES = (WAYPOINT_MISSION_NAME, BOTTLE_DROP_MISSION_NAME, TARGET_SEARCH_MISSION_NAME, OTHER_MISSION_NAME, LAND_MISSION_NAME)
 
@@ -50,24 +56,48 @@ class Application(Frame):
 
     def generateTapped(self):
         mission_name, mission_id = self.getDropdownData()
+        interop_data = self.getMissionWithId(mission_id).mission # If mission id not valid, will return everything but waypoints
 
-        # First, check if we need to generate points now
+        # First, check if we need to generate waypoints
         if mission_name == TARGET_SEARCH_MISSION_NAME:
-            search_area_bounds = self.getMissionWithId(mission_id)
             # Now generate a search path
+            pass
 
         elif mission_name in INTEROP_SOURCE:
-            result = self.getMissionWithId(mission_id)
-            mission_to_generate = result # Pass straight through
-            self.setDataLabel(result)
+            # Send to mission_planner immediately
+            pass
 
         else:
             # Read points from file
-            pass
+            filename = askopenfilename(title="Select file of waypoints") # show an "Open" dialog box and return the path to the selected file
+            if not filename:
+                return
 
+            with open(filename) as f:
+                lines = f.readlines()
+                i = 0
+                for line in lines:
+                    data = line.split(',')
+                    data = [float(d.strip()) for d in data]
+                    p = uav_msgs.msg.Point()
+                    p.latitude = data[0]
+                    p.longitude = data[1]
+                    p.altitude = data[2]
 
+                    op = uav_msgs.msg.OrderedPoint()
+                    op.point = p
+                    op.ordinal = i
+                    interop_data.waypoints.append(op)
+
+                    i += 1
+
+        self.setDataLabel(interop_data)
+        self.generatePath(interop_data)
+        self.setState(PATH_GENERATED_STATE)
+            
     def sendTapped(self):
-        pass
+        self.uploadPath()
+        self.setState(PATH_SEND_STATE)
 
     def cancelTapped(self):
         pass
@@ -77,6 +107,20 @@ class Application(Frame):
     
     def pauseResumeTapped(self):
         self.switchPauseResumeState() 
+
+    def setState(self, newState):
+        self.state = newState
+        self.statusLabel.configure(text=self.state)
+        
+        if self.state == INIT_STATE:
+            self.generatePathButton.configure(state=NORMAL)
+            self.sendPathButton.configure(state=DISABLED)
+        elif self.state == PATH_GENERATED_STATE:
+            self.generatePathButton.configure(state=NORMAL)
+            self.sendPathButton.configure(state=NORMAL)
+        elif self.state == PATH_SEND_STATE:
+            self.generatePathButton.configure(state=NORMAL)
+            self.sendPathButton.configure(state=NORMAL)
 
     def setDataLabel(self, data):
         self.dataLabel.configure(state=NORMAL)
@@ -99,13 +143,15 @@ class Application(Frame):
         self.getMissionWithId = rospy.ServiceProxy("get_mission_with_id", uav_msgs.srv.GetMissionWithId)
         print("Found service\n")
 
-        #print("Waiting for 'send_waypoints' service to become available")
-        #rospy.wait_for_service("send_waypoints")
-        self.uploadPath = rospy.ServiceProxy("send_waypoints", uav_msgs.srv.UploadPath)
+        print("Waiting for 'send_waypoints' service to become available")
+        rospy.wait_for_service("theseus/send_waypoints")
+        self.uploadPath = rospy.ServiceProxy("theseus/send_waypoints", uav_msgs.srv.UploadPath)
+        print("Found service\n")
 
-        #print("Waiting for 'plan_mission' service to become available")
-        #rospy.wait_for_service("plan_mission")
-        self.generatePath = rospy.ServiceProxy("plan_mission", uav_msgs.srv.GeneratePath)
+        print("Waiting for 'plan_mission' service to become available")
+        rospy.wait_for_service("theseus/plan_mission")
+        self.generatePath = rospy.ServiceProxy("theseus/plan_mission", uav_msgs.srv.GeneratePath)
+        print("Found service\n")
 
     def createWidgets(self):
         self.mpSection = LabelFrame(self.master, text="Mission Planning", pady=20)
@@ -117,26 +163,31 @@ class Application(Frame):
         self.chosenMissionDropdown.configure(width=10)
         self.chosenMissionDropdown.grid(row=0, column=0, padx=10, pady=10)
 
-        self.generatePathButton = Button(self.mpSection, text="Generate Path", command=self.generateTapped, state=NORMAL)
+        self.generatePathButton = Button(self.mpSection, text="Generate Path", command=self.generateTapped)
         self.generatePathButton.grid(row=0, column=1, padx=10, pady=10)
 
-        self.sendPathButton = Button(self.mpSection, text="Send Path", command=self.sendTapped, state=DISABLED)
+        self.sendPathButton = Button(self.mpSection, text="Send Path", command=self.sendTapped)
         self.sendPathButton.grid(row=0, column=2, padx=10, pady=10)
 
 
         self.mcSection = LabelFrame(self.master, text="Mission Control", pady=10)
         self.mcSection.grid(padx=10, pady=10)
 
-        self.statusLabel = Label(self.mcSection, text="Status")
-        self.statusLabel.grid(row=0, column=0, padx=10, pady=10)
+        self.statusText = Label(self.mcSection, text="Status")
+        self.statusText.grid(row=0, column=0, padx=10, pady=10)
 
-        self.pauseResumeButton = Button(self.mcSection, width=10, text=self.pauseResumeState, command=self.pauseResumeTapped, state=NORMAL)
+        self.statusLabel = Label(self.mcSection)
+        self.statusLabel.grid(row=0, column=1, padx=10, pady=10)
+        self.setState(INIT_STATE)
+
+
+        self.pauseResumeButton = Button(self.mcSection, width=10, text=self.pauseResumeState, command=self.pauseResumeTapped, state=DISABLED)
         self.pauseResumeButton.grid(row=1, column=0, padx=10, pady=10)
 
-        self.cancelButton = Button(self.mcSection, text="Cancel", command=self.cancelTapped, state=NORMAL)
+        self.cancelButton = Button(self.mcSection, text="Cancel", command=self.cancelTapped, state=DISABLED)
         self.cancelButton.grid(row=1, column=1, padx=10, pady=10)
 
-        self.restartButton = Button(self.mcSection, text="Restart", command=self.restartTapped, state=NORMAL)
+        self.restartButton = Button(self.mcSection, text="Restart", command=self.restartTapped, state=DISABLED)
         self.restartButton.grid(row=1, column=2, padx=10, pady=10)
 
 
