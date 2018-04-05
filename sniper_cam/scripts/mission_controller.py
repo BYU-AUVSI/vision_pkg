9ed08ac4e6ef2242765feecb2c65b1b44125d6c1
Ephraim Kunz <ephraimkunz@me.com>
2018-04-05 09:07:09 -0600

    Integrate lawnmower path into mission planner

--- sniper_cam/scripts/mission_controller.py
+++ sniper_cam/scripts/mission_controller.py
@@ -9,6 +9,8 @@ import signal
 import uav_msgs.msg
 import uav_msgs.srv
 from rosplane_msgs.msg import *
+import shapely.geometry
+import pyproj
 
 PAUSE = "Pause"
 RESUME = "Resume"
@@ -58,12 +60,28 @@ class Application(Frame):
     def generateTapped(self):
         mission_name, mission_id = self.getDropdownData()
         interop_data = self.getMissionWithId(mission_id).mission # If mission id not valid, will return everything but waypoints
-        print(interop_data)
-        
+
         # First, check if we need to generate waypoints
         if mission_name == TARGET_SEARCH_MISSION_NAME:
             # Now generate a search path
-            pass
+            points = self.generateLawnmowerPath(interop_data)
+            new_waypoints = []
+            i = 1
+            for point in points:
+                op = uav_msgs.msg.OrderedPoint()
+                op.ordinal = i
+
+                p = uav_msgs.msg.Point()
+                p.latitude = point.y
+                p.longitude = point.x
+                p.altitude = 40
+                op.point = p
+
+                new_waypoints.append(op)
+
+                i += 1
+            
+            interop_data.waypoints = new_waypoints
 
         elif mission_name in INTEROP_SOURCE:
             # Send to mission_planner immediately
@@ -93,7 +111,8 @@ class Application(Frame):
 
                     i += 1
 
-        self.generatePath(interop_data)
+        self.setDataLabel(interop_data)
+        result = self.generatePath(interop_data)
         self.setState(PATH_GENERATED_STATE)
             
     def sendTapped(self):
@@ -139,16 +158,6 @@ class Application(Frame):
         self.pauseResumeButton.configure(text=self.pauseResumeState)
 
     def setupServices(self):
-        # TODO: For testing : make path planner happy. This should be commented out when running in the field
-        # pub = rospy.Publisher('state', State, queue_size=10)
-        # state = State()
-        # state.initial_lat = 38.143264
-        # state.initial_lon = -76.43075
-        # state.initial_alt = 6.7
-        # print(state)
-        # print(pub)
-        # pub.publish(state)
-
         print("Waiting for 'get_mission_with_id' service from interop client.py to become available")
         rospy.wait_for_service("get_mission_with_id")
         self.getMissionWithId = rospy.ServiceProxy("get_mission_with_id", uav_msgs.srv.GetMissionWithId)
@@ -208,6 +217,92 @@ class Application(Frame):
         self.dataLabel = ScrolledText(root, width=50, height=20)
         self.dataLabel.grid(padx = 10, pady=10)
 
+    # TODO: Clean up this code (remove duplication)
+    def generateLawnmowerPath(self, mission):
+        # Set up projections
+        p_ll = pyproj.Proj(init='epsg:4326')
+        p_mt = pyproj.Proj(init='epsg:3857') # metric; same as EPSG:900913
+
+        # Perimeter of points
+        boundaries = mission.waypoints
+        perimeter = [(p.point.longitude, p.point.latitude) for p in boundaries]
+
+        polygon = shapely.geometry.Polygon(perimeter)
+
+        max_lat = perimeter[0][1]
+        max_lon = perimeter[0][0]
+        min_lat = perimeter[0][1]
+        min_lon = perimeter[0][0]
+        for p in perimeter:
+            if p[0] > max_lon:
+                max_lon = p[0]
+            if p[0] < min_lon:
+                min_lon = p[0]
+            if p[1] > max_lat:
+                max_lat = p[1]
+            if p[1] < min_lat:
+                min_lat = p[1]
+
+        # Upper left and lower right corner of bounding box
+        nw = shapely.geometry.Point((min_lon, max_lat))
+        se = shapely.geometry.Point((max_lon, min_lat))
+
+        front_to_back_step_size = 100 # 1000 = 1 km grid step size
+        side_to_side_step_size = 100
+
+        # Project corners to target projection
+        s = pyproj.transform(p_ll, p_mt, nw.x, nw.y) # Transform NW point to 3857
+        e = pyproj.transform(p_ll, p_mt, se.x, se.y) # .. same for SE
+
+        # Determine long flight direction
+        horiz = abs(s[0] - e[0])
+        vert = abs(s[1] - e[1])
+        if vert > horiz:
+            # Iterate over 2D area
+            forwards = True
+            gridpoints = []
+            x = s[0]
+            while x < e[0]:
+                if forwards:
+                    y = s[1]
+                    while y > e[1]:
+                        p = shapely.geometry.Point(pyproj.transform(p_mt, p_ll, x, y))
+                        gridpoints.append(p)
+                        y -= front_to_back_step_size
+                else:
+                    y = e[1]
+                    while y < s[1]:
+                        p = shapely.geometry.Point(pyproj.transform(p_mt, p_ll, x, y))
+                        gridpoints.append(p)
+                        y += front_to_back_step_size
+
+                forwards = not forwards
+                x += side_to_side_step_size
+
+        else:
+            # Iterate over 2D area
+            forwards = True
+            gridpoints = []
+            y = s[1]
+            while y > e[1]:
+                if forwards:
+                    x = s[0]
+                    while x < e[0]:
+                        p = shapely.geometry.Point(pyproj.transform(p_mt, p_ll, x, y))
+                        gridpoints.append(p)
+                        x += front_to_back_step_size
+                else:
+                    x = e[0]
+                    while x > s[0]:
+                        p = shapely.geometry.Point(pyproj.transform(p_mt, p_ll, x, y))
+                        gridpoints.append(p)
+                        x -= front_to_back_step_size
+
+                forwards = not forwards
+                y -= side_to_side_step_size
+
+        inside = [p for p in gridpoints if p.within(polygon)]
+        return inside
 
 # create the application and run it
 root = Tk()
